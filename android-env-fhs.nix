b6fc87bcf13b48ab5ea451df88dcb34feaf19823
EndCredits <alicization.han@gmail.com>
2022-09-26 07:23:31 +0800

    dotfiles: Initial
    Signed-off-by: EndCredits <alicization.han@gmail.com>

---
+++ android-env-fhs.nix
@@ -0,0 +1,49 @@
+{ pkgs ? import <nixpkgs> {} }:
+ 
+let fhs = pkgs.buildFHSUserEnv {
+  name = "android-env";
+  targetPkgs = pkgs: with pkgs;
+    [ git
+      gitRepo
+      gnupg
+      python2
+      curl
+      procps
+      openssl
+      gnumake
+      nettools
+      androidenv.androidPkgs_9_0.platform-tools
+      jdk
+      schedtool
+      utillinux
+      m4
+      gperf
+      perl
+      libxml2
+      zip
+      unzip
+      bison
+      flex
+      lzop
+      python3
+      android-tools
+    ];
+  multiPkgs = pkgs: with pkgs;
+    [ zlib
+      ncurses5
+    ];
+  runScript = "bash";
+  profile = ''
+    export ALLOW_NINJA_ENV=true
+    export USE_CCACHE=1
+    export ANDROID_JAVA_HOME=${pkgs.jdk.home}
+    export LD_LIBRARY_PATH=/usr/lib:/usr/lib32
+    export PATH=/home/crepuscular/Working/home/crepuscular/toolchains/clang-crepuscular/bin:$PATH
+  '';
+};
+in pkgs.stdenv.mkDerivation {
+  name = "android-env-shell";
+  nativeBuildInputs = [ fhs ];
+  shellHook = "exec android-env";
+
+}
