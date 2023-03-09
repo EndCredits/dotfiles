f24c65207e84e0a32cce1f3002c73dc9ad88eee9
EndCredits <alicization.han@gmail.com>
2023-03-09 23:21:48 +0800

    kernel-builder: Drop QT dependencies
     * We don't need to use xconfig in kernel so just remove it
    
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- kernel-build-env.nix
+++ kernel-build-env.nix
@@ -7,16 +7,12 @@ in
     [
       pkgconfig
       ncurses
-      qt5.qtbase
       pkgsCross.aarch64-multiplatform.stdenv.cc
     ]
     ++ pkgs.linux.nativeBuildInputs);
   runScript = pkgs.writeScript "init.sh" ''
     export ARCH=arm64
     export CROSS_COMPILE=aarch64-unknown-linux-gnu-
-    export PKG_CONFIG_PATH="${pkgs.ncurses.dev}/lib/pkgconfig:${pkgs.qt5.qtbase.dev}/lib/pkgconfig"
-    export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins"
-    export QT_QPA_PLATFORMTHEME=qt5ct
     exec bash
   '';
 }).env
