b9abb797a678b00d05856bebf49f180545b76960
EndCredits <alicization.han@gmail.com>
2023-03-10 19:33:09 +0800

    kernel-builder: Add more build dependencies
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- kernel-build-env.nix
+++ kernel-build-env.nix
@@ -6,13 +6,24 @@ in
   targetPkgs = pkgs: (with pkgs;
     [
       pkgconfig
-      ncurses
+      ncurses6
+      ncurses5
       pkgsCross.aarch64-multiplatform.stdenv.cc
+      pkgsCross.arm-embedded.stdenv.cc
+      clang15Stdenv
+      pkgsLLVM.crossLibcStdenv
+      glibc
+      libcxx
+      zlib
+      ninja
     ]
     ++ pkgs.linux.nativeBuildInputs);
   runScript = pkgs.writeScript "init.sh" ''
     export ARCH=arm64
-    export CROSS_COMPILE=aarch64-unknown-linux-gnu-
+    export LOCAL_CLANG_PATH=$HOME/Development/clang-google/linux-x86/clang-r487747
+    export PATH=$LOCAL_CLANG_PATH/bin:$PATH
+    export CLANG_TRIPLE=aarch64-linux-gnu-
+    export LD_LIBRARY_PATH=$LOCAL_CLANG_PATH/lib:$LD_LIBRARY_PATH
     exec bash
   '';
 }).env
